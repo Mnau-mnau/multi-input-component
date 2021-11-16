a711146dc519f522d5e6bf5e631ae746bbbdba68
Natalie <natalie.mikesova@oracle.com>
2021-11-16 10:05:07 +0100

    refactor: presentation placement of the component

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MultipleInputHooks />
+    <div className="presentationPlacement">
+      <MultipleInputHooks />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
